9fc30d35ef62c6bdeece85443411ce9c6bdb0128
Raghav3398 <108568552+Raghav3398@users.noreply.github.com>
2024-06-28 09:57:57 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use stirct"; // treat all JS code as newer version
+
+// alert(3 + 3) // we aer using nodejs , not browser
+
+console.log(3 + 3)
+
+console.log("Raghav");
+
+
+let name = "Raghav"
+let age = 17
+let isLoggedIn = false    
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);  // object
