8b462a4fddfa2cc59b80c726f554acf3eed55c24
Raghav3398 <108568552+Raghav3398@users.noreply.github.com>
2024-06-30 17:46:14 +0000

    controlflowOne

---
+++ 04_control_flow/one.js
@@ -0,0 +1,39 @@
+// if
+const isUserLogggedIn = true
+const temperature = 41
+
+// if (temperature === 40){
+// console.log("less than 50");
+// } else{
+//     console.log("temperature is less than 50");
+// }
+
+// console.log("Executed");
+// <, >, <=, >=, ==, !=, ===, !==   
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 7510");
+// } else if (balance < 900) {
+
+//     console.log("less than 900");
+
+// }else {
+//     console.log("less than 1200");
+
+// }
