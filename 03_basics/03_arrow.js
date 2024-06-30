0ee3e6b93905adced7689094c5ee76b4659fe9e8
Raghav3398 <108568552+Raghav3398@users.noreply.github.com>
2024-06-30 12:00:38 +0000

    arrowfunction

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,49 @@
+const user = {
+    username: "Raghav",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username}, welcome to this website`);
+        console.log(this);
+    }
+}
+
+    // user.welcomeMessage()
+    // user.username = "Sam"
+    // user.welcomeMessage()
+
+    // console.log(this);
+
+    // function chai(){
+    //     let username = "Raghav"
+    //     console.log(this.username);
+    // }
+
+    // chai()
+
+    // const chai = function(){
+    //     let username = "Raghav"
+    //     console.log(this.username);
+    // }
+
+    const chai = () => {
+        let username = "Raghav"
+        console.log(this);
+    }
+
+    // chai()
+
+    // const addTwo = (num1, num2) => {
+    //     return num1 + num2 
+    // }
+    
+    // const addTwo = (num1, num2) => num1 + num2
+    
+    // const addTwo = (num1, num2) => (num1 + num2)
+    
+    const addTwo = (num1, num2) => ({username: "Raghav"})
+    console.log(addTwo(3, 4));
+
+    const myArray = [2, 5, 3, 7, 8]
+
+        myArray.forEach()
