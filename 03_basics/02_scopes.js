ee6d9a5b313f994a62019a5907deec973c376256
Raghav3398 <108568552+Raghav3398@users.noreply.github.com>
2024-06-30 11:14:41 +0000

    02_scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,54 @@
+// var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER:", a);
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "Raghav"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if (true) {
+    const username = "Raghav"
+    if (username === "Raghav") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// +++++++++++++++++++++ interesting ++++++++++++++++++++++++
+
+addone(5)
+
+function addone(num){
+    return num + 1
+}
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
